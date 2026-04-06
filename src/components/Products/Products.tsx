6f9877eeb3cddb53c6629282bdf6f1b213edf948
mznmo <102366479+mznmo@users.noreply.github.com>
2026-04-06 18:20:51 +0200

    reponsiveness

--- src/components/Products/Products.tsx
+++ src/components/Products/Products.tsx
@@ -5,19 +5,18 @@ import { useCart } from "../../store/CartContext";
 import { motion } from "framer-motion";
 
 type ProductsProps = {
-  filterType: string; // declaring the type of filterType as string
+  filterType: string;
 };
 
 export default function Products({ filterType }: ProductsProps) {
   const token = localStorage.getItem("token");
-  console.log(filterType);
-  const { addToCart } = useCart(); // using the custom hook to get the addToCart function from CartContext
+  const { addToCart } = useCart();
 
   const filteredWatches = filterType
     ? filterType === "All"
-      ? watches //if filtertype is all then show all watches
-      : watches.filter((watch) => watch.category === filterType) //if filtertype is not all then show the category
-    : watches; //if not selected category then show all
+      ? watches
+      : watches.filter((watch) => watch.category === filterType)
+    : watches;
 
   return (
     <motion.div className="grid mt-6" viewport={{ once: true }}>
@@ -32,25 +31,31 @@ export default function Products({ filterType }: ProductsProps) {
         >
           <li>
             <Link to={`/watch/${watch.id}`}>
-              <img src={watch.image[0]} alt={watch.name} />
+              <img
+                src={watch.image[0]}
+                alt={watch.name}
+                className="w-full object-cover"
+              />
               <motion.div
-                className="absolute bottom-2/12 left-1/12 p-4 text-white"
+                className="absolute bottom-2/12 left-1/12 p-2 sm:p-4 text-white"
                 initial={{ opacity: 0, y: 30 }}
                 whileInView={{ opacity: 1, y: 0 }}
                 transition={{ duration: 0.5, delay: 0.3 }}
                 viewport={{ once: true }}
               >
-                <h2 className="font-bold mb-5 text-lg sm:text-4xl">
+                <h2 className="font-bold mb-2 sm:mb-5 text-base sm:text-2xl lg:text-4xl">
                   {watch.name}
                 </h2>
-                <p className="font-thin text-normal sm:text-xl">
+                <p className="hidden sm:block font-thin text-sm lg:text-xl ">
                   {watch.description[0]}
                 </p>
-                <p className="font-thin">${watch.price.toLocaleString()}</p>
+                <p className="font-thin text-xs sm:text-base">
+                  ${watch.price.toLocaleString()}
+                </p>
               </motion.div>
             </Link>
             <motion.div
-              className="absolute bottom-0.5 sm:bottom-1/12 left-1/12 p-4"
+              className="absolute bottom-0.5 sm:bottom-1/12 left-1/12 p-2 sm:p-4"
               initial={{ opacity: 0, y: 30 }}
               whileInView={{ opacity: 1, y: 0 }}
               transition={{ duration: 0.5, delay: 0.4 }}
